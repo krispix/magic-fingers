c600e3491690598855aa9ba07f2cdfc4d92ad93b
Kristin <kurisuu@gmail.com>
2015-02-27 16:47:45 -0600

    fixed shit

--- evaluategrades.c
+++ evaluategrades.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #define SIZE 5
 
+
 int main(int argc, char *argv[])
 {
     int grade[SIZE];
@@ -13,34 +14,84 @@ int main(int argc, char *argv[])
     int value_b = 0;
     int value_c = 0;
     
-    printf("Enter five, integer grades.\n\n");
+    printf("Enter five, integer grades:\n\n");
     scanf("%d", &value);
     
-    for (index = 0; index < SIZE-1 && value >= 70 && value <= 100; index++)
-    {
-        grade[index] = value;
-        sum = sum + value;
-        if (value >= 70 && value < 80)
-        {
-                  value_c = value_c++;  // count C grades
-                  }
-                  else if (value >= 80 && value < 90)
-                  {
-                       value_b = value_b++;  // count B grades
-                       }
-                       else if (value >= 90 && value <= 100)
-                       {
-                            value_a = value_a++;  // count A grades
+    
+    if (value >= 70 && value <= 100) {
+              grade[0] = value;
+              sum = sum + value;
+              index++;
+              
+              for (index = 1; index < (SIZE-1) && value >= 70 && value <= 100; index++) {
+                  
+                  if (value >= 70 && value <= 79) {
+                            grade[index] = value;
+                            sum = sum + value;
+                            value_c++;  // count C grades
                             }
+                            
+                            else if (value >= 80 && value <= 89) {
+                                 grade[index] = value;
+                                 sum = sum + value;
+                                 value_b++;  // count B grades
+                                 }
+                                 
+                                 else if (value >= 90 && value <= 100) {
+                                      grade[index] = value;
+                                      sum = sum + value;
+                                      value_a++;  // count A grades
+                                      }
+                                      
+                                      else if (value < 70) {
+                                           printf("Grade must be greater than 70.\n\n");
+                                           }
+                                           
+                                           else if (value > 100) {
+                                                printf("Grade must be less than 100.\n\n");
+                                                }
                 
         scanf("%d", &value);
+        
         }
         
+        if (value >= 70 && value <= 79) {
+                  grade[index] = value;
+                  sum = sum + value;
+                  value_c++;  // count C grades
+                  }
+                  
+                  else if (value >= 80 && value <= 89) {
+                       grade[index] = value;
+                       sum = sum + value;
+                       value_b++;  // count B grades
+                       }
+                       
+                       else if (value >= 90 && value <= 100) {
+                            grade[index] = value;
+                            sum = sum + value;
+                            value_a++;  // count A grades
+                            }
+                            
+                            else if (value < 70) {
+                                 printf("Grade must be greater than 70.\n\n");
+                                 }
+                                 
+                                 else if (value > 100) {
+                                      printf("Grade must be less than 100.\n\n");
+                                      }
+        
         avg = sum / (SIZE-1);  // average grade
+        
         printf("\nThe average grade is:  %d\n\n", avg);
         printf("The number of A's is %d\n\n", value_a);
         printf("The number of B's is %d\n\n", value_b);
         printf("The number of C's is %d\n\n", value_c);
+        }
+        
+        else {
+             printf("Grades must be integers between 70 and 100.\n\n");
+             }
   
   system("PAUSE");	
   return 0;
